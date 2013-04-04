a71c61df5d815456d984b3e34a8439215d282532
gnysek <524094+gnysek@users.noreply.github.com>
2013-04-04 22:59:07 +0200

    - checking that ID is valid before closing Edit form - edit button enabling/disabling if items on grid selected - changed way of rendering item list, to prevent code duplication

--- ItemPacker2013/Edit.cs
+++ ItemPacker2013/Edit.cs
@@ -6,14 +6,40 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace ItemPacker2013
 {
 	public partial class Edit : Form
 	{
+		public int EditID = -1;
+
 		public Edit()
 		{
 			InitializeComponent();
+		}
+
+		private void Edit_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			int tmpID = -1;
+			int.TryParse(itemID.Text, out tmpID);
+
+			Regex match = new Regex("^[0-9]+$");
+
+			if (tmpID == -1 || !match.IsMatch(itemID.Text))
+			{
+				itemID.BackColor = Color.Red;
+				e.Cancel = true;
+				return;
+			}
+
+			if (MainForm.CurrentProject.itemCollection.ContainsKey(tmpID) && EditID != tmpID)
+			{
+				itemID.BackColor = Color.Red;
+				e.Cancel = true;
+				MessageBox.Show("There is already item with that ID");
+				return;
+			}
 		}
 	}
 }
