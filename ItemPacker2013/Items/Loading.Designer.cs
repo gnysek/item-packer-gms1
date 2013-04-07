f1210647f8d8ac308233040f04a1008278e8f40b
gnysek <524094+gnysek@users.noreply.github.com>
2013-04-07 19:43:06 +0200

    - loading window - not modal for now - preload image sprites

---
+++ ItemPacker2013/Items/Loading.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ItemPacker2013.Items
+{
+	partial class Loading
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.progressBar = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// progressBar
+			// 
+			this.progressBar.Location = new System.Drawing.Point(12, 12);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(260, 16);
+			this.progressBar.TabIndex = 0;
+			// 
+			// Loading
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 40);
+			this.ControlBox = false;
+			this.Controls.Add(this.progressBar);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.Name = "Loading";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Loading";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		protected System.Windows.Forms.ProgressBar progressBar;
+
+	}
+}
